a20762813a22e284733412722cc3db094914ee24
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-07-06 18:33:51 -0400

    added p-tuning script

--- p_tuning_checkpoint.py
+++ p_tuning_checkpoint.py
@@ -131,7 +131,7 @@ with DAG(
     )
 
     t3 = PythonOperator(
-            task_id = 'download nemo ckpt',
+            task_id = 'download_nemo_checkpoint',
             python_callable= download_nemo_checkpoint,
             op_kwargs= {"org":org_, "team": team_, "ace": ace_},
             dag = dag
