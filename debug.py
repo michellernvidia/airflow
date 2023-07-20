5a54c7e2eeb6542ed9710ea3e5f5103818417c42
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-07-20 11:39:30 -0500

    checking token

--- debug.py
+++ debug.py
@@ -55,7 +55,7 @@ def wait_for_job_completion(ti, org, team=None):
 
      min=0
      while job_status != 'FINISHED_SUCCESS' and job_status != 'FAILED' and job_status != 'KILLED_BY_USER':
-         time.sleep(300) #increase wait time to 5 mins
+         time.sleep(30) #increase wait time to 5 mins
          min += 5
          job_status = ngc_job_status(ti, org, job_id)
          print(f'minute: {min} | Job status: ', job_status)
