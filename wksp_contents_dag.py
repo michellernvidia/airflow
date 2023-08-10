fe120f9278ede5fcf4e198dc587758df9cdbe6eb
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-08-10 17:51:16 -0400

    fixes

--- wksp_contents_dag.py
+++ wksp_contents_dag.py
@@ -52,88 +52,15 @@ with DAG(
             op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "workspace_name": gpt_workspace_name},
             dag = dag)
     
-    create_tuning_workspace_task = PythonOperator(
-            task_id = 'create_tuning_workspace',
-            python_callable= create_task_workspace,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "workspace_name": tuning_workspace_name},
-            dag = dag)
-    
     get_wksp_contents_task = PythonOperator(
             task_id = 'get_wksp_contents',
             python_callable= get_workspace_contents,
             op_kwargs= {"ngc_api_key": key_, "org":org_, "workspace_id": "Wm4074mZQ46Mr3u-NaLkXw"},
             dag = dag)
-    
-    pretrain_decision_task = BranchPythonOperator(
-            task_id='get_base_model',
-            provide_context=True,
-            python_callable=get_base_model,
-            op_kwargs={"pretrain_decision": pretrain_decision_},
-            dag=dag)
-    
-    download_checkpoint_task = PythonOperator(
-            task_id = 'download_nemo_checkpoint',
-            python_callable= download_nemo_checkpoint,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "nemo_ckpt_file": nemo_ckpt_, "team": team_},
-            dag = dag)
-
-    # op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "workspace_name": workspace_name_, "team": team_},
-    download_the_pile_task = PythonOperator(
-            task_id = 'download_pile_dataset',
-            python_callable= download_pile_dataset,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_},
-            dag = dag)
-
-    train_gpt_task = PythonOperator(
-            task_id = 'train_gpt_model',
-            python_callable= train_gpt_model,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_},
-            dag = dag)
-    
-    download_squad_task = PythonOperator(
-            task_id = 'download_squad_dataset',
-            python_callable=get_squad_dataset,
-            op_kwargs={"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_, "tuning_method": tuning_method_},
-            trigger_rule=TriggerRule.ONE_SUCCESS, 
-            dag=dag
-    )
-
-    choose_tuning_task = BranchPythonOperator(
-            task_id = 'choose_tuning_method',
-            python_callable=choose_tuning_method,
-            op_kwargs={"method": tuning_method_},
-            dag=dag
-    )
-
-    p_tuning_train_task = PythonOperator(
-            task_id = 'p_tuning_train',
-            python_callable= p_tuning_training_bcp,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_},
-            dag = dag)
-    
-    p_tuning_inference_task = PythonOperator(
-            task_id = 'p_tuning_inference',
-            python_callable= p_tuning_inference_bcp,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_},
-            dag = dag)
-
-    lora_train_task = PythonOperator(
-            task_id = 'LoRA_train',
-            python_callable= lora_training_bcp,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_},
-            dag = dag)
-    
-    lora_inference_task = PythonOperator(
-            task_id = 'LoRA_inference',
-            python_callable= lora_inference_bcp,
-            op_kwargs= {"ngc_api_key": key_, "org":org_, "ace": ace_, "team": team_},
-            dag = dag)
 
 
-create_gpt_workspace_task >> pretrain_decision_task
-create_tuning_workspace_task >> pretrain_decision_task
 
-pretrain_decision_task >> get_wksp_contents_task
+create_gpt_workspace_task >> get_wksp_contents_task
 
 # pretrain_decision_task >> [download_checkpoint_task, download_the_pile_task]
 # download_the_pile_task >> train_gpt_task >> download_squad_task
