de855128058d2e565207eff45b9c95d767e94d6d
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-08-26 22:20:36 -0400

    page size param - 300

--- ngc_requests.py
+++ ngc_requests.py
@@ -73,7 +73,7 @@ def get_workspace_contents(ngc_api_key, org, workspace_id):
     token = get_token(ngc_api_key, org)
 
     url = f'https://api.ngc.nvidia.com/v2/org/{org}/workspaces/{workspace_id}/listFiles'
-    params={'flat-dir': True, 'page-size': 200}
+    params={'flat-dir': True, 'page-size': 300}
     headers = {'Content-Type': 'application/json',
                'Authorization': f'Bearer {token}'}
 
