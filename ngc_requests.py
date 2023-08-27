06ab55fb7cdce378ca4ff293cc3a3ba6a36cabe2
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-08-26 21:45:05 -0400

    printing out params

--- ngc_requests.py
+++ ngc_requests.py
@@ -74,6 +74,7 @@ def get_workspace_contents(ngc_api_key, org, workspace_id):
 
     url = f'https://api.ngc.nvidia.com/v2/org/{org}/workspaces/{workspace_id}/listFiles'
     params={'flat-dir': True, 'Page Size': 200}
+    print('params: ', params)
     headers = {'Content-Type': 'application/json',
                'Authorization': f'Bearer {token}'}
 
