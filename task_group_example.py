84795b0d2c6a1c713ea62c6fb86218f1df28c791
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-08-28 21:23:16 -0400

    task group example

--- task_group_example.py
+++ task_group_example.py
@@ -1,13 +1,26 @@
 from airflow.decorators import task_group
 from airflow.operators import EmptyOperator
 
+import datetime
 
-@task_group()
-def group1():
-    task1 = EmptyOperator(task_id="task1")
-    task2 = EmptyOperator(task_id="task2")
+from airflow import DAG
+from airflow.decorators import task_group
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dag1",
+    start_date=datetime.datetime(2016, 1, 1),
+    schedule="@daily",
+    default_args={"retries": 1},
+):
+
+    @task_group()
+    def group1():
+        task1 = EmptyOperator(task_id="task1")
+        task2 = EmptyOperator(task_id="task2")
 
 
-task3 = EmptyOperator(task_id="task3")
+    task3 = EmptyOperator(task_id="task3")
 
 group1() >> task3
