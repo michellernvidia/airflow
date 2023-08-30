75b5dcb6c94bcfe59b9f80e4e3aa8ee6279782cd
Michelle Ramirez <micheller@micheller-mlt.nvidia.com>
2023-08-30 10:50:28 -0400

    cleaning task_workspace.py

--- task_workspace.py
+++ task_workspace.py
@@ -1,17 +1,19 @@
-from ngc_requests import *
+from ngc_requests import get_existing_workspace, create_workspace
 
 
 def create_task_workspace(ti, ngc_api_key, org, ace, workspace_name):
+    '''Creates a NGC workspace in the specied NGC org and ace under the name *workspace_name*.
+    Returns workspace ID for created workspace to be used in downstream tasks.'''
+    
     #check if workspace exists already
-    print('checking if wksp exists...')
     workspace_response=get_existing_workspace(ti, ngc_api_key, org, workspace_name)
     
-    #if not, create new one
+    #if not, create a new one
     if workspace_response["requestStatus"]["statusCode"] == 'NOT_FOUND':
-        print('workspace does not exist', workspace_name)
+        print(f'Workspace does not exist. Creating workspace {workspace_name}.')
         workspace_response = create_workspace(ti, ngc_api_key, org, ace, workspace_name)
     else:
-        print('workspace exists already.. getting id..')
+        print(f'Workspace {workspace_name} exists already.')
     
     workspace_id = workspace_response['workspace']['id']
     return workspace_id
